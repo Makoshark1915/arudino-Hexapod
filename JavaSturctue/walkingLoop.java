06613fc1aeb3b8c91a46ce3b1a7fa9c122d0cd2a
Makoshark1915 <131227094+Makoshark1915@users.noreply.github.com>
2026-03-12 13:59:01 +0000

    Add initial implementation of walkingLoop class with basic structure

---
+++ JavaSturctue/walkingLoop.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
+
+
+public class walkingLoop {
+  static Scanner input;
+
+    boolean group1Move = true;
+
+
+    String[][] legs = {{"L1X", "L1Y", "L1Z", "L1C"}, {"L2X", "L2Y", "L2Z", "L2C"}, {"L3X", "L3Y", "L3Z", "L3C"},
+     {"L4X", "L4Y", "L4Z", "L4C"}, {"L5X", "L5Y", "L5Z", "L5C"}, {"L6X", "L6Y", "L6Z", "L6C"}};
+
+
+
+
+
+
+
+
+
+  public static void main(String[] args) {
+    input = new Scanner(System.in);
+    
+ 
+ 
+ 
+ 
+  }
+
+
+
+
+}
