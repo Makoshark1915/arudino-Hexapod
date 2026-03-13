67f271d516d1e258ea77d3325f47829867d0bc23
Makoshark1915 <131227094+Makoshark1915@users.noreply.github.com>
2026-03-13 08:37:25 +0000

    Refactor walkingLoop class to improve joystick input handling and leg position management

--- JavaSturctue/walkingLoop.java
+++ JavaSturctue/walkingLoop.java
@@ -1,4 +1,3 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 
@@ -7,28 +6,81 @@ import java.util.Scanner;
 public class walkingLoop {
   static Scanner input;
 
-    boolean group1Move = true;
+    
+    int step = 0; // variable to keep track of which step in the walking cycle we are on, will be updated every loop
+
+  
+    final float[][] legsDefault = {{132.95f, 90, 60}, {132.95f, 90, 60}, {132.95f, 90, 60}, 
+    {132.95f, 90, 60}, {132.95f, 90, 60}, {132.05f, 90, 60} }; // default position for all 6 legs, will be used to set legs to a default "standing" position
 
 
-    String[][] legs = {{"L1X", "L1Y", "L1Z", "L1C"}, {"L2X", "L2Y", "L2Z", "L2C"}, {"L3X", "L3Y", "L3Z", "L3C"},
-     {"L4X", "L4Y", "L4Z", "L4C"}, {"L5X", "L5Y", "L5Z", "L5C"}, {"L6X", "L6Y", "L6Z", "L6C"}};
+    float[][] legs // moving position for all 6 legs, will be updated every loop  in the format of legs{legnumber}{x,y,z}
+    = legsDefault;
 
 
-
-
-
-
-
-
+    final float mult = 1; // multiplier for how much the joystick input affects movement
 
   public static void main(String[] args) {
     input = new Scanner(System.in);
     
- 
- 
- 
- 
+    while (true) {
+
+      System.out.println("Enter the xy joystick values (x y): ");
+      String joystickInput = input.nextLine();
+      
+
+
+      float[] Input = new float[2];
+      Input[0] = Float.parseFloat(joystickInput.split(" ")[0]);
+      Input[1] = Float.parseFloat(joystickInput.split(" ")[1]);
+
+
+
+      System.out.println();
+      System.out.println("Joystick Values: " + Input[0] + ", " + Input[1]);
+
+
+
+      Step(Input);
+
+
+
+
+    }
   }
+
+  static void Step(float[] Input) 
+  {
+
+    float joystickX = Input[0];
+    float joystickY = Input[1];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
+
 
 
 
