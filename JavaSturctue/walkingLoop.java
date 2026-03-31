5506a7e07ba86eb64bab6f3890b198f5dea42ebf
Makoshark1915 <131227094+Makoshark1915@users.noreply.github.com>
2026-03-31 14:11:26 +0000

    first full commit of the walkingLoop.java code

--- JavaSturctue/walkingLoop.java
+++ JavaSturctue/walkingLoop.java
@@ -6,17 +6,14 @@ import java.util.Scanner;
 public class walkingLoop {
   static Scanner input;
 
-    
-    static int step = 0; // variable to keep track of which step in the walking cycle we are on, will be updated every loop
-
   
-    final static float[][] legsDefault = {{132.95f, 60.0f, 70.0f}, {132.95f, 60.0f, 70.0f}, {132.95f, 60.0f, 70.0f}, 
-    {132.95f, 60.0f, 70.0f}, {132.95f, 60.0f, 70.0f}, {132.95f, 60.0f, 70.0f} }; // default position for all 6 legs, will be used to set legs to a default "standing" position
+    final static float[][] legsDefault = {{132.95f, 80.0f, 90.0f}, {132.95f, 80.0f, 90.0f}, {132.95f, 80.0f, 90.0f} }; // default position for all 6 legs, will be used to set legs to a default "standing" position
 
 
-    static float[][] legs // moving position for all 6 legs, will be updated every loop  in the format of legs{legnumber}{x,y,z}
+    static float[][] legsGroup1 // moving position for all 6 legs, will be updated every loop  in the format of legs{legnumber}{x,y,z}
     = legsDefault;
-
+    static float[][] legsGroup2 // moving position for all 6 legs, will be updated every loop  in the format of legs{legnumber}{x,y,z}
+    = legsDefault;
 
     static final float mult = 1; // multiplier for how much the joystick input affects movement
 
@@ -24,10 +21,17 @@ public class walkingLoop {
     static float joystickY; // variable to store the y value of the joystick input, will be updated every loop
 
 
-    
+/* leg setup
 
+      front
+    1-1   2-1   
 
+  2-2       1-2
 
+    1-3   2-3
+       back
+
+*/
   public static void main(String[] args) {
     input = new Scanner(System.in);
     
@@ -49,35 +53,10 @@ public class walkingLoop {
 
 
 
-      while(joystickX != 0 || joystickY != 0){ // if the joystick is not in the neutral position, run the walking cycle
-       
-       if (step >1) { // if we have completed the walking cycle, reset the step variable to 0 to start the cycle over again
-          step = 0;
-        } 
-      
-        if(step == 0){ 
-          step0(joystickX, joystickY);    
-        }
+      if(!joystickAtRest()){ // if the joystick is not in the neutral position, run the walking cycle
 
-
-
-
-        else if(step == 1){ // if we are in the first step of the walking cycle, run the code for the first step
-          step1(joystickX, joystickY);
-        }
-    
+          moving(joystickX, joystickY);
         
-
-
-
-        else {// if we are in any other step, break the loop (this should never happen, but is here as a safety measure) 
-          
-          System.out.println("Error: Invalid step in walking cycle. Step: " + step);        
-          break;  
-        }
-
-        step++; // update the step variable to move to the next step in the walking cycle   
-
       }
 
 
@@ -92,43 +71,185 @@ public class walkingLoop {
   
 
 
-  static void step0(float joystickX, float joystickY) { //run the init step of walking cycle: lift or drop
-
-    legs[0][2] = 0;
-    legs[3][2] = 0;
-    legs[4][2] = 0;
 
 
+static void moving(float joystickX, float joystickY) { // run the first step of walking cycle
 
 
-
-  }
+   
 
 
 
+  
 
-  static void step1(float joystickX, float joystickY) { // run the first step of walking cycle: move
+    if(groupUp() == 0 || groupUp() == 1){
 
+    while(inRange() && legsGroup1[0][2] >= legsDefault[0][2] && !joystickAtRest()){ // while the legs are in range and the first leg group is up, move the legs according to the joystick input
 
-
-    legs[1][1] += joystickY * mult;
-    legs[2][1] += joystickY * mult;
-    legs[5][1] += joystickY * mult;
-
-    legs[1][0] += joystickX * mult;
-    legs[2][0] += joystickX * mult;
-    legs[5][0] += joystickX * mult;
-
-
-  }
-
-
-
-
-
-
-  static void reCenter() { // run re-center the legs to the default position
+      legsGroup1[0][0] += joystickX * mult;
+      legsGroup1[0][1] += joystickY * mult; // joystickY for all of these might need to be flipped/have changed signs
  
+      legsGroup1[1][0] += joystickX * mult;
+      legsGroup1[1][1] += joystickY * mult;
+
+      legsGroup1[2][0] += joystickX * mult;
+      legsGroup1[2][1] += joystickY * mult;
+
+
+      legsGroup2[0][0] -= joystickX * mult;
+      legsGroup2[0][1] -= joystickY * mult;
+
+      legsGroup2[1][0] -= joystickX * mult;
+      legsGroup2[1][1] -= joystickY * mult;
+
+      legsGroup2[2][0] -= joystickX * mult;
+      legsGroup2[2][1] -= joystickY * mult;
+
+
+    }
   }
+  else if(groupUp() == 2){
+
+
+    while(inRange() && legsGroup2[0][2] >= legsDefault[0][2] && !joystickAtRest()){ // while the legs are in range and the first leg group down, move the legs according to the joystick input
+
+      legsGroup1[0][0] -= joystickX * mult;
+      legsGroup1[0][1] -= joystickY * mult; // joystickY for all of these might need to be flipped/have changed signs
+ 
+      legsGroup1[1][0] -= joystickX * mult;
+      legsGroup1[1][1] -= joystickY * mult;
+
+      legsGroup1[2][0] -= joystickX * mult;
+      legsGroup1[2][1] -= joystickY * mult;
+
+
+      legsGroup2[0][0] += joystickX * mult;
+      legsGroup2[0][1] += joystickY * mult;
+
+      legsGroup2[1][0] += joystickX * mult;
+      legsGroup2[1][1] += joystickY * mult;
+
+      legsGroup2[2][0] += joystickX * mult;
+      legsGroup2[2][1] += joystickY * mult;
+
+
+    }
+  }
+  else{
+    while (true){
+    System.out.println("ERROR bad read on Scenario");
+    }
+  }
+
+
+
+
+
+
+
+
+  }
+
+static void reCenter() { // run re-center the legs to the default position
+ 
+}
+
+static void applyChanges() { // apply the changes to the legs
+
+    
+}
+
+static boolean inRange() { // check's if the giver xyz values for all legs are in a reachable position
+
+
+
+    boolean inRange = true; 
+
+
+
+    return inRange;
+  }
+
+static boolean joystickAtRest() { // check if the joystick is in the neutral position
+
+
+  boolean atRest = false;
+
+  if(joystickX == 0 && joystickY == 0){
+    atRest = true;
+  }
+
+  return atRest;
+}
+
+static int groupUp(){// checks which leg group is raised
+
+  
+  
+  int scenario;
+  
+  if(legsGroup1==legsDefault && legsGroup2==legsDefault){//at default group 1 moves up, scenario 0
+  
+      legsGroup2[0][2] = legsDefault[0][2];
+      legsGroup2[1][2] = legsDefault[1][2];
+      legsGroup2[2][2] = legsDefault[2][2];
+      applyChanges();
+
+
+      legsGroup1[0][2] = legsDefault[0][2]-10;
+      legsGroup1[1][2] = legsDefault[1][2]-10;
+      legsGroup1[2][2] = legsDefault[2][2]-10;
+      applyChanges();
+
+      scenario = 0;
+      
+    }
+    else if(legsGroup1[0][2] == legsDefault[0][2] && legsGroup2[0][2] == legsDefault[0][2]-10){ //group 1 moves up group 2 moves down, scenario 1
+
+      legsGroup2[0][2] = legsDefault[0][2];
+      legsGroup2[1][2] = legsDefault[1][2];
+      legsGroup2[2][2] = legsDefault[2][2];
+      applyChanges();
+
+      legsGroup1[0][2] = legsDefault[0][2]-10;
+      legsGroup1[1][2] = legsDefault[1][2]-10;
+      legsGroup1[2][2] = legsDefault[2][2]-10;
+      applyChanges();
+
+      scenario = 1;
+
+    } 
+    else if(legsGroup2[0][2] == legsDefault[0][2] && legsGroup1[0][2] == legsDefault[0][2]-10){ //group 1 moves down group 2 moves up, scenario 2
+
+      legsGroup1[0][2] = legsDefault[0][2];
+      legsGroup1[1][2] = legsDefault[1][2];
+      legsGroup1[2][2] = legsDefault[2][2];
+      applyChanges();
+
+      legsGroup2[0][2] = legsDefault[0][2]-10;
+      legsGroup2[1][2] = legsDefault[1][2]-10;
+      legsGroup2[2][2] = legsDefault[2][2]-10;
+      applyChanges();
+
+      scenario = 2;
+
+    }
+    else
+    {    
+       while (true){
+
+        System.out.println("ERROR bad Scenario");
+
+        } // if in none of the previous options something broke run ERROR
+    }
+
+
+
+    return scenario;
+}
+
+
+
+
 
 }
